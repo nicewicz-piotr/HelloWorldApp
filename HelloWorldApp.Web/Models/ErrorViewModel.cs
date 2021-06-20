bce5e0987ebb36829e7bf814d78425b8a03ac0dd
Piotr Nicewicz <nicewicz.piotr@gmail.com>
2021-06-20 12:24:03 +0200

    Test change

--- HelloWorldApp.Web/Models/ErrorViewModel.cs
+++ HelloWorldApp.Web/Models/ErrorViewModel.cs
@@ -4,7 +4,7 @@ namespace HelloWorldApp.Web.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string RequestId { get; set; }z
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
